ad89ecc12c1bb16568267a5a1f6016473d6ff61e
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2023-12-12 16:03:05 +0800

    Update FruitGenerator.cs

--- Assets/Scripts/FruitGenerator.cs
+++ Assets/Scripts/FruitGenerator.cs
@@ -17,10 +17,11 @@ public class FruitGenerator : MonoBehaviour
     public float LimitLeft = -9.5f;
     [Header("右邊界"), Tooltip("這是水果出現最右邊的位置限制")]
     public float LimitRight = 9.5f;
+    int random = 0;
 
     private void Awake()
     {
-        int random = UnityEngine.Random.Range(0, 3); // 隨機生成 Lv0 ~ Lv3 的水果
+        random = UnityEngine.Random.Range(0, 3); // 隨機生成 Lv0 ~ Lv3 的水果
         InitialPosition = new Vector3(UnityEngine.Random.Range(LimitLeft, LimitRight), 7f, 0f);
         transform.position = InitialPosition;
         for (int i = 0; i < FruitLevel.Length; i++) // 開始時將所有物件關閉
@@ -43,7 +44,6 @@ public class FruitGenerator : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
-    // 穿透類型的碰撞器被碰了
     private void OnTriggerEnter(Collider other)
     {
         // 如果玩家碰到我
@@ -51,11 +51,26 @@ public class FruitGenerator : MonoBehaviour
         {
             SaveManager.instance.Score += 1;
             print("<color=#00ff00>碰到玩家了</color>");
-            /*SaveManager.instance.gem++;
-            AudioManager.instance.Play("寶石");
-            SaveManager.instance.hp += 1;*/
+            /*
+             * SaveManager.instance.Score += pow(2, random);
+             * AudioManager.instance.Play("吃到水果");
+             */
+            Destroy(this.gameObject); // 刪除自己的遊戲物件
+        }
+    }
+
+    // 穿透類型的碰撞器被碰了
+    /*private void OnTriggerEnter(Collider other)
+    {
+        // 如果玩家碰到我
+        if (other.tag == "Player")
+        {
+            SaveManager.instance.Score += 1;
+            print("<color=#00ff00>碰到玩家了</color>");
+            // SaveManager.instance.Score += pow(2, random);
+            // AudioManager.instance.Play("吃到水果");
             Destroy(this.gameObject); // 刪除自己的遊戲物件
         }
 
-    }
+    }*/
 }
