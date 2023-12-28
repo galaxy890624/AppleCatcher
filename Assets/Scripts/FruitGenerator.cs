594c07aafa5338e562ca7bed2ef5d202188f7a3b
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2023-12-29 00:47:41 +0800

    update FruitGenerator.cs

--- Assets/Scripts/FruitGenerator.cs
+++ Assets/Scripts/FruitGenerator.cs
@@ -4,6 +4,32 @@ using UnityEngine;
 
 public class FruitGenerator : MonoBehaviour
 {
+    [Header("水果等級")] // 必須由 Lv 0 排到 Lv 7
+    public GameObject[] FruitLevel = null;
+    public float DropSpeed = 2.0f;
+    [Header("左邊界"), Tooltip("這是水果出現最左邊的位置限制")]
+    public float LimitLeft = -9.5f;
+    [Header("右邊界"), Tooltip("這是水果出現最右邊的位置限制")]
+    public float LimitRight = 9.5f;
+
+    public GameObject Generator = null;
+
+    [Header("放生成物位置")]
+    public Transform GeneratePosition;
+
+    int random = 0; // global
+
+    private GameObject RandomFruit()
+    {
+        random = Random.Range(0, 3);
+        return FruitLevel[random];
+    }
+    // Initialize
+    private void OnEnable() // 開始使用所有gameObject
+    {
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
