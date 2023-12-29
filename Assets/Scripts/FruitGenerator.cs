0d7aed0d87612b378ab701ac426025625087c4b1
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2023-12-30 04:52:29 +0800

    update FruitGenerator.cs

--- Assets/Scripts/FruitGenerator.cs
+++ Assets/Scripts/FruitGenerator.cs
@@ -35,7 +35,10 @@ public class FruitGenerator : MonoBehaviour
     {
         
     }
-
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
     // Update is called once per frame
     void Update()
     {
