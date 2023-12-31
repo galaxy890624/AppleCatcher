0a1df8839aee31ff5fe8bf2fe11c858734db9b39
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-01-01 03:03:16 +0800

    update FruitGenerator.cs

--- Assets/Scripts/FruitGenerator.cs
+++ Assets/Scripts/FruitGenerator.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class FruitGenerator : MonoBehaviour
 {
+    // initialize
+    private void OnEnable()
+    {
+        
+    }
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+    }
     // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        
+    }
+    // decommit
+    private void OnDisable()
     {
         
     }
