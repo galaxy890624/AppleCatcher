8664f43d2eed89a35b44751dec7b0cc882092d48
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-01-01 03:45:17 +0800

    update Prefab

--- Assets/Scripts/FruitGenerator.cs
+++ Assets/Scripts/FruitGenerator.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class FruitGenerator : MonoBehaviour
 {
+    [Header("水果等級")] // 必須由 Lv 0 排到 Lv 7
+    public GameObject[] FruitLevel = null;
+    [Header("左邊界"), Tooltip("這是水果出現最左邊的位置限制")]
+    public float LimitLeft = -9.5f;
+    [Header("右邊界"), Tooltip("這是水果出現最右邊的位置限制")]
+    public float LimitRight = 9.5f;
+
     // initialize
     private void OnEnable()
     {
