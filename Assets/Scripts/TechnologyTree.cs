4e8554cc9cd42b0627c646575ddfa39e79346a1e
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-01-06 18:11:07 +0800

    update TechnologyTree

---
+++ Assets/Scripts/TechnologyTree.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class TechnologyTree : MonoBehaviour
+{
+    public void Â÷¶}()
+    {
+        Debug.Log("Â÷¶}");
+        SceneManager.LoadScene("Menu");
+    }
+}
