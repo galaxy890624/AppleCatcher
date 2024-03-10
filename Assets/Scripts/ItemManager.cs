a6d9112c112b451657a800f33f05dd1310abb1e3
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-03-11 03:06:01 +0800

    Update ItemManager.cs

---
+++ Assets/Scripts/ItemManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Progress;
+
+// 用來控制Item的儲存
+// 參考ChatboxManager.cs
+
+[CreateAssetMenu(fileName = "ItemManager", menuName = "Item/ItemManager")] // 右鍵 > Chatbox > ChatboxManager
+
+public class ItemManager : ScriptableObject
+{
+    /// <summary>
+    /// public List<列表型態> 列表名稱
+    /// 列表裡, "List<>"內的資料型態 必須和 "想要指定的.cs檔案" 一致
+    /// ex:
+    /// public List<Item> itemList = new List<Item>();
+    /// </summary>
+    public List<Item> itemList = new List<Item>();
+}
