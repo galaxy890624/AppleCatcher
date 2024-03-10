6a2f96730d2ef0152298ead9ecac6c4bd81696b8
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-03-11 02:22:10 +0800

    Update ..\Object

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 用來儲存水果的資訊
+// ex: 水果動畫, index, 得分, ...
+// 參考Chatbox.cs
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item/New Item")] // 右鍵 > Item > New Item
+
+public class Item : ScriptableObject
+{
+    public int Index;
+    public string ItemName;
+    public Sprite ItemImage; // 頭像圖片
+    public int GetScore; // 吃到後得到的分數
+    public float ItemQuantity; // 持有數量
+
+}
