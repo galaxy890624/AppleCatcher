c276c52f953a9f8995533fbd60c01bbd9f9ccd00
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-03-22 14:51:42 +0800

    Update ItemGenerator.cs

--- Assets/Scripts/ItemGenerator.cs
+++ Assets/Scripts/ItemGenerator.cs
@@ -104,3 +104,32 @@ public class ChatboxGenerator : MonoBehaviour
     }
 }
 */
+
+/*
+ * public class FruitGenerator : MonoBehaviour
+{
+    [Header("水果等級")] // 必須由 Lv 0 排到 Lv 7
+    public GameObject[] FruitLevel = null; // 所有水果的Prefabs
+    // Vector3 InitialPosition;
+    [Header("左邊界"), Tooltip("這是水果出現最左邊的位置限制")]
+    public float LimitLeft = -9.5f;
+    [Header("右邊界"), Tooltip("這是水果出現最右邊的位置限制")]
+    public float LimitRight = 9.5f;
+    [Header("生成水果的位置")]
+    public Transform Generator;
+    int random = 0;
+    float SpawnTime = 0f; // 生成水果的計時器
+
+    // Game logic
+    private void Update()
+    {
+        if (Time.time > SpawnTime)
+        {
+            random = UnityEngine.Random.Range(0, 3); // 隨機生成 Lv0 ~ Lv2 的水果
+            Generator.position = new Vector3(UnityEngine.Random.Range(LimitLeft, LimitRight), 7f, 0f);
+            Instantiate(FruitLevel[random], Generator.position, Quaternion.identity);
+            SpawnTime += 1f; // Delay 1秒
+        }
+    }
+}
+ */
