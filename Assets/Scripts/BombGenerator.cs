b874c091fb00a44f42e130d071eff8baba6b7e2d
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-04-04 23:04:01 +0800

    Update BombGenerator.cs

--- Assets/Scripts/BombGenerator.cs
+++ Assets/Scripts/BombGenerator.cs
@@ -21,7 +21,7 @@ public class BombGenerator : MonoBehaviour
     {
         if (Time.timeSinceLevelLoad > SpawnTime) // 為了中途進入遊玩場景, 不要從一進遊戲就開始累積炸彈, 所以不能用Time.time
         {
-            random = UnityEngine.Random.Range(0, 3); // 隨機生成 Lv0 ~ Lv2 的水果
+            //random = UnityEngine.Random.Range(0, 3); // 隨機生成 Lv0 ~ Lv2 的水果
             Generator.position = new Vector3(UnityEngine.Random.Range(LimitLeft, LimitRight), 7f, 0f);
             Instantiate(Bomb[random], Generator.position, Quaternion.identity);
             SpawnTime += 1f; // Delay 1秒
