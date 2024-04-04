0d3943ab6c973099d9999a0dde9cc1aa418233c5
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-04-04 23:40:49 +0800

    Update BombCatcher.cs

--- Assets/Scripts/BombCatcher.cs
+++ Assets/Scripts/BombCatcher.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class BombCatcher : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] Bomb = null;
+    public Item Item; // 要先實例化才能用
+
+    // initialize
+    private void OnEnable()
     {
-        
+        // 確保 Item 已經被實例化
+        if (Item == null)
+        {
+            // 實例化 Item
+            // Item = ScriptableObject.CreateInstance<Item>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Physics
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.tag == "Player")
+        {
+            print($"<color=#ff00ff>我接到<color=#00ff00>{gameObject.name}</color>了唷!</color>");
+            print($"{UnityEditor.ArrayUtility.IndexOf<GameObject>(Bomb, gameObject)}"); // Input = ..\Prefabs\Cherries ; Output = 2
+            // 吃到炸彈 數量就 + 1
+            Item.ItemQuantity += 1;
+
+            SaveManager.instance.Score -= (UnityEditor.ArrayUtility.IndexOf<GameObject>(Bomb, gameObject) + 1); // 扣分
+            Destroy(gameObject);
+        }
+        else if (collision.tag == "Ground")
+        {
+            print($"<color=#ff00ff><color=#00ff00>{gameObject.name}</color>掉在地上了唷!</color>");
+            Destroy(gameObject);
+        }
     }
 }
