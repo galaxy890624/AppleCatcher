5d06e650c6c5be0acab7a1cfa63fb474b9edf91b
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-04-14 02:36:09 +0800

    Update HPMonitor.cs

--- Assets/Scripts/HPMonitor.cs
+++ Assets/Scripts/HPMonitor.cs
@@ -6,6 +6,7 @@ public class HPMonitor : MonoBehaviour
 {
     [Header("血量顯示器的位置")]
     public Transform Parent;
+    [SerializeField] List<GameObject> 血量UI = new List<GameObject>(); // 血量的愛心物件
     
     private void Start()
     {
@@ -16,14 +17,13 @@ public class HPMonitor : MonoBehaviour
     {
         SaveManager.instance.愛心變化事件 -= 刷新顯示;
     }
-    [SerializeField] List<GameObject> 血量UI = new List<GameObject>(); // 血量的愛心物件
 
     void 刷新顯示()
     {
         for (int i = 0; i < transform.childCount; i++)
         {
+            print($"<color=#ff00ff>已刪除第<color=#00ff00>{i}</color>個愛心 : <color=#00ff00>{transform.GetChild(0).name}</color></color>");
             Destroy(transform.GetChild(0).gameObject); // 用for迴圈, 每次都刪除第1個(index = 0)子物件
-            print($"<color=#ff00ff>已刪除第<color=#00ff00>{i}</color>個愛心</color>");
         }
         for (int i = 0; i < SaveManager.instance.血量; i++)
         {
