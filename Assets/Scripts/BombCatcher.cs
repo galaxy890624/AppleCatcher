ac24d17ac0ec48acfdb6d4add19b21324b4f8635
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-04-14 00:56:20 +0800

    Update BombCatcher.cs

--- Assets/Scripts/BombCatcher.cs
+++ Assets/Scripts/BombCatcher.cs
@@ -23,8 +23,8 @@ public class BombCatcher : MonoBehaviour
     {
         if (collision.tag == "Player")
         {
-            print($"<color=#ff00ff>我接到<color=#00ff00>{gameObject.name}</color>了唷!</color>");
-            print($"{UnityEditor.ArrayUtility.IndexOf<GameObject>(Bomb, gameObject)}"); // Input = ..\Prefabs\Cherries ; Output = 2
+            //print($"<color=#ff00ff>我接到<color=#00ff00>{gameObject.name}</color>了唷!</color>");
+            //print($"{UnityEditor.ArrayUtility.IndexOf<GameObject>(Bomb, gameObject)}"); // Input = ..\Prefabs\Cherries ; Output = 2
             // 吃到炸彈 數量就 + 1
             Item.ItemQuantity += 1;
 
@@ -35,7 +35,7 @@ public class BombCatcher : MonoBehaviour
         }
         else if (collision.tag == "Ground")
         {
-            print($"<color=#ff00ff><color=#00ff00>{gameObject.name}</color>掉在地上了唷!</color>");
+            //print($"<color=#ff00ff><color=#00ff00>{gameObject.name}</color>掉在地上了唷!</color>");
             Destroy(gameObject);
         }
     }
