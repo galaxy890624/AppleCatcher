afc64f4533029ef2f8c023d6de7e36e1b4ec114e
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-04-14 05:16:41 +0800

    Update DataSkill.cs

---
+++ Assets/Scripts/DataSkill.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataSkill : ScriptableObject
+{
+    [Header("技能名稱")]
+    public string SkillName;
+    [Header("技能等級"), Range(0, 20)]
+    public int Lv = 0;
+    [Header("技能每個等級能力值")]
+    public float[] SkillValues = null;
+
+    // Lambda 運算子 (簡寫)
+    // Lambda 符號後可以運算或寫程式
+
+    /// <summary>
+    /// 目前技能的能力值
+    /// </summary>
+    public float SkillValue => SkillValues[Lv - 1];
+}
