2f5545415d6119fb4c26aa297ead50ffc64f043c
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-04-14 06:20:43 +0800

    Update Upgrade_MoveSpeed.cs

---
+++ Assets/Scripts/Upgrade_MoveSpeed.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 升級技能 : 玩家移動速度
+/// </summary>
+/// 
+public class Upgrade_MoveSpeed : UpgradeLevelUp // 參考 Upgrade_4_PlayerHorizontalSpeed
+{
+
+}
