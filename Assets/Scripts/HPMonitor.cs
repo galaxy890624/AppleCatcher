9e19b3f1c889dec1a6afd0878a0c1747a29bf4f3
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-04-14 12:46:18 +0800

    Update HPMonitor.cs

--- Assets/Scripts/HPMonitor.cs
+++ Assets/Scripts/HPMonitor.cs
@@ -7,7 +7,16 @@ public class HPMonitor : MonoBehaviour
     [Header("血量顯示器的位置")]
     public Transform Parent;
     [SerializeField] List<GameObject> 血量UI = new List<GameObject>(); // 血量的愛心物件
-    
+
+    private void Awake()
+    {
+        for (int i = 0; i < SaveManager.instance.血量; i++)
+        {
+            Instantiate(血量UI[i], new Vector3(-14f + i, 7f, 0f), Quaternion.identity, Parent); // 跟隨Stage的座標
+            print($"<color=#ff00ff>已生成第<color=#00ff00>{i}</color>個愛心</color>");
+        }
+    }
+
     private void Start()
     {
         SaveManager.instance.愛心變化事件 += 刷新顯示;
@@ -20,10 +29,6 @@ public class HPMonitor : MonoBehaviour
 
     void 刷新顯示()
     {
-        for (int i = 0; i < SaveManager.instance.血量; i++)
-        {
-            Instantiate(血量UI[i], new Vector3(-14f + i, 7f, 0f), Quaternion.identity, Parent); // 跟隨Stage的座標
-            print($"<color=#ff00ff>已生成第<color=#00ff00>{i}</color>個愛心</color>");
-        }
+        
     }
 }
