a77dd8fe1dfafc5b02d9b1c9b92dbb5f70be7251
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-04-14 11:31:59 +0800

    Update HPMonitor.cs

--- Assets/Scripts/HPMonitor.cs
+++ Assets/Scripts/HPMonitor.cs
@@ -20,11 +20,6 @@ public class HPMonitor : MonoBehaviour
 
     void 刷新顯示()
     {
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            print($"<color=#ff00ff>已刪除第<color=#00ff00>{i}</color>個愛心 : <color=#00ff00>{transform.GetChild(0).name}</color></color>");
-            Destroy(transform.GetChild(0).gameObject); // 用for迴圈, 每次都刪除第1個(index = 0)子物件
-        }
         for (int i = 0; i < SaveManager.instance.血量; i++)
         {
             Instantiate(血量UI[i], new Vector3(-14f + i, 7f, 0f), Quaternion.identity, Parent); // 跟隨Stage的座標
